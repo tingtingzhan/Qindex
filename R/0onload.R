6c96c7d1bc1a1de16a57539ea7cfb46ee5d51990
Tingting Zhan <tingtingzhan@gmail.com>
2025-02-15 11:40:44 -0500

    test

---
+++ R/0onload.R
@@ -0,0 +1,10 @@
+
+
+.onLoad <- function(libname, pkgname = 'Qindex') {
+  
+  library(spatstat.grouped)
+  library(maxEff) # previous [optim_split_etc].
+  library(boc) # previous [BBC_dichotom] function 
+  library(gam.matrix) # previous [Qindex] function
+  
+}
